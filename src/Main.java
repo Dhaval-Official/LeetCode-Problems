f8f5845b0717930e03dfe99dd1157fda49a2834a
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-09 22:35:49 +0530

    New Code Space for leetCode Problems commit trial

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello and welcome!");
+        System.out.println("New Code Space for leetCode Problems ");
 
-        for (int i = 1; i <= 5; i++) {
 
-            System.out.println("i = " + i);
-            System.out.println();
-        }
     }
 }
