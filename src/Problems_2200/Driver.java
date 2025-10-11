57ab01dcec6f53d6fc76110a8ea73358dd231e9a
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-11 22:36:27 +0530

    LeetCode Problem 2236

--- src/Problems_2200/Driver.java
+++ src/Problems_2200/Driver.java
@@ -6,10 +6,14 @@ public class Driver {
     public static void main(String[] args) {
 
         TreeNode t1 = new TreeNode(4);
-        TreeNode t2 = new TreeNode(6);
+        TreeNode t2 = new TreeNode(1);
         TreeNode t3 = new TreeNode(10, t1, t2);
 
+        Solution s = new Solution();
 
+        boolean a = s.checkTree(t3);
+
+        System.out.println(a);
 
 
     }
--- src/Problems_2200/Problem_36.java
+++ src/Problems_2200/Problem_36.java
@@ -1,4 +1,13 @@
 package Problems_2200;
 
-public class Problem_36 {
+import common.TreeNode;
+
+class Solution {
+    public boolean checkTree(TreeNode root) {
+
+        int a = root.val;
+
+        return root.val == root.left.val + root.right.val;
+
+    }
 }
