6ba22a0d89def2516bc5ff6e74a482ffcc6f6b0d
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-12 22:36:30 +0530

    New Naming convention

--- src/Problems/Problems_2200/Solution_2236.java
+++ src/Problems/Problems_2200/Solution_2236.java
@@ -6,7 +6,7 @@ class Solution_2236 {
     public boolean checkTree(TreeNode root) {
 
         int a = root.val;
-
+        System.out.println();
         return root.val == root.left.val + root.right.val;
 
     }
