684a3e51e51fb92d05066ede445599961a835e97
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-13 22:10:14 +0530

    Problem 0009 Runtime A++ Memorytime A+

--- src/Problems/Problems_0000/Solution_0009.java
+++ src/Problems/Problems_0000/Solution_0009.java
@@ -5,6 +5,33 @@ public class Solution_0009 {
 
         if(x < 0) {return false;}
 
+        int R,L;
+        int newLeft;
+        L = x;
+
+        newLeft = 0;
+
+
+        while (L > 0){
+
+            R = L%10;
+            newLeft = newLeft*10 + R;
+//            System.out.println(L+" "+R+" "+newLeft);
+
+            L = L/10;
+
+        };
+//        System.out.println(x);
+//        System.out.println(newLeft);
+        return (x == newLeft);
+    }
+
+    /*this is more Optimized version on Leetcode
+    *
+    * public boolean isPalindrome(int x) {
+
+        if(x < 0) {return false;}
+
         int L = x;
         int y = 0;
 
@@ -14,5 +41,5 @@ public class Solution_0009 {
         };
 
         return (x == y);
-    }
+    }*/
 }
