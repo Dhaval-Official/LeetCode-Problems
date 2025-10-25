8fab22caaed33e918096ea72b40f14130cbc59e1
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-25 22:14:14 +0530

    Problem 0009 retest

--- src/Problems/Problems_0000/Solution_0014.java
+++ src/Problems/Problems_0000/Solution_0014.java
@@ -1,7 +1,22 @@
 package Problems.Problems_0000;
 
 public class Solution_0014 {
-    public static void main(String[] args) {
 
+    public String longestCommonPrefix(String[] strs) {
+
+        StringBuilder common = new StringBuilder("");
+        String s = strs[0];
+
+        common = new StringBuilder(s);
+
+        for (int i = 0; i < strs.length-1; i++) {
+            for (int j = 0; j < common.length()-1; j++) {
+//                if()
+            }
+        }
+
+
+        return "";
     }
+
 }
