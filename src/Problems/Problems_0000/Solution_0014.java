44089f1f0711cf557d04412c4c8caa924d7216bc
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-29 21:57:58 +0530

    Problem 14

--- src/Problems/Problems_0000/Solution_0014.java
+++ src/Problems/Problems_0000/Solution_0014.java
@@ -4,19 +4,29 @@ public class Solution_0014 {
 
     public String longestCommonPrefix(String[] strs) {
 
+        StringBuffer realcommon = new StringBuffer("");
         String s = strs[0];
         char common;
 //        common = new StringBuilder(s);
+        boolean diffFound = false;
 
         for (int i = 0; i < strs[0].length()-1; i++) {
             common = strs[0].charAt(i);
             for (int j=1;j<strs.length-1;j++) {
-//                if(common != )
+                if(common != strs[j].charAt(i)) {
+                    diffFound = true;
+                    break;
+                }
+            }
+            if(diffFound) {
+                break;
+            } else {
+                realcommon.append(common);
             }
         }
 
 
-        return "";
+        return realcommon.toString();
     }
 
 }
