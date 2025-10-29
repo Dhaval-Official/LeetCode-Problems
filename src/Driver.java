508f2d7b3b9ef8ae24887cd9c0b426ba8555a718
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-29 22:36:35 +0530

    Problem 14

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,14 @@
 import Problems.Problems_0000.Solution_0009;
+import Problems.Problems_0000.Solution_0014;
 
 public class Driver {
 
     public static void main(String[] args) {
-        Solution_0009 s1 = new Solution_0009();
 
-        ;
-        System.out.println(s1.isPalindrome(923432));
+        String[] arr = {"dog","dog","dog","do"};
 
-        Solution_0009 sw = new Solution_0009();
-        System.out.println(sw.isPalindrome(923432));
-
-        Solution_0009 s2 = new Solution_0009();
-        System.out.println(s2.isPalindrome(923432));
-
-        Solution_0009 s3 = new Solution_0009();
-        System.out.println(s3.isPalindrome(923432));
+        Solution_0014 s1 = new Solution_0014();
+        System.out.println(s1.longestCommonPrefix(arr));
 
     }
 }
--- src/Problems/Problems_0000/Solution_0014.java
+++ src/Problems/Problems_0000/Solution_0014.java
@@ -4,20 +4,30 @@ public class Solution_0014 {
 
     public String longestCommonPrefix(String[] strs) {
 
+        if(strs.length == 1) {return strs[0];}
+
         StringBuffer realcommon = new StringBuffer("");
         String s = strs[0];
         char common;
 //        common = new StringBuilder(s);
         boolean diffFound = false;
 
-        for (int i = 0; i < strs[0].length()-1; i++) {
+        for (int i = 0; i < strs[0].length(); i++) {
             common = strs[0].charAt(i);
-            for (int j=1;j<strs.length-1;j++) {
-                if(common != strs[j].charAt(i)) {
-                    diffFound = true;
-                    break;
-                }
+
+            for (int j=1;j<strs.length;j++) {
+                System.out.println(common + " " + strs[j].charAt(i));
+                if(i<strs[j].length()-1) {
+                    if (common != strs[j].charAt(i)) {
+                        diffFound = true;
+                        break;
+                    }
+//                } else {
+//                    diffFound = true;
+//                    break;
+//                }
             }
+
             if(diffFound) {
                 break;
             } else {
