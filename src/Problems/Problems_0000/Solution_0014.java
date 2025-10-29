5a49c068ecd156d23cb92c5a64d7102e63632af7
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-29 22:42:54 +0530

    Problem 14 Runtime 1 ms Beats 58.21% Memory 41.58 MB Beats 63.80%

--- src/Problems/Problems_0000/Solution_0014.java
+++ src/Problems/Problems_0000/Solution_0014.java
@@ -6,28 +6,25 @@ public class Solution_0014 {
 
         if(strs.length == 1) {return strs[0];}
 
-        StringBuffer realcommon = new StringBuffer("");
+        StringBuilder realcommon = new StringBuilder();
         String s = strs[0];
         char common;
-//        common = new StringBuilder(s);
         boolean diffFound = false;
 
         for (int i = 0; i < strs[0].length(); i++) {
             common = strs[0].charAt(i);
 
             for (int j=1;j<strs.length;j++) {
-                System.out.println(common + " " + strs[j].charAt(i));
-                if(i<strs[j].length()-1) {
+                if(i<strs[j].length()) {
                     if (common != strs[j].charAt(i)) {
                         diffFound = true;
                         break;
                     }
-//                } else {
-//                    diffFound = true;
-//                    break;
-//                }
+                } else {
+                    diffFound = true;
+                    break;
+                }
             }
-
             if(diffFound) {
                 break;
             } else {
