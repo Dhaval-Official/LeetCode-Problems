c95779477d48f6a7821c1ec5e9d5aa60dba70272
Dhaval <dhavallakhaniofficial@gmail.com>
2025-10-29 21:52:56 +0530

    Problem 14

--- src/Problems/Problems_0000/Solution_0014.java
+++ src/Problems/Problems_0000/Solution_0014.java
@@ -4,14 +4,14 @@ public class Solution_0014 {
 
     public String longestCommonPrefix(String[] strs) {
 
-        StringBuilder common = new StringBuilder("");
         String s = strs[0];
+        char common;
+//        common = new StringBuilder(s);
 
-        common = new StringBuilder(s);
-
-        for (int i = 0; i < strs.length-1; i++) {
-            for (int j = 0; j < common.length()-1; j++) {
-//                if()
+        for (int i = 0; i < strs[0].length()-1; i++) {
+            common = strs[0].charAt(i);
+            for (int j=1;j<strs.length-1;j++) {
+//                if(common != )
             }
         }
 
