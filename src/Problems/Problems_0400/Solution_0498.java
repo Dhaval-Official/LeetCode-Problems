49dbb76f977c5a975585cff67aed85180a218d42
Dhaval <dhavallakhaniofficial@gmail.com>
2025-11-19 23:10:00 +0530

    Problem 0498 Done Runtime 2 ms Beats 99.14%
    Memory
    47.72 MB Beats 32.54%

--- src/Problems/Problems_0400/Solution_0498.java
+++ src/Problems/Problems_0400/Solution_0498.java
@@ -3,14 +3,12 @@ package Problems.Problems_0400;
 public class Solution_0498 {
     int[] ans;
     public int[] findDiagonalOrder(int[][] mat) {
-
         // int total = mat.length*mat[0].length;
         ans = new int[mat.length*mat[0].length];
 
         int ptr = 0;
         int i=0,j=0,imax=mat.length-1,jmax=mat[0].length-1;
         ans[ptr++] = mat[0][0];
-
         while (ptr < mat.length*mat[0].length) {
 
             if(i == 0 && j%2 == 0 || j == jmax) {
