dbc569d33e8be023e1785d263b10bc8c573be395
Dhaval <dhavallakhaniofficial@gmail.com>
2025-11-22 22:43:51 +0530

    Problem 0021 Done

--- src/Problems/Problems_0000/Solution_0021.java
+++ src/Problems/Problems_0000/Solution_0021.java
@@ -1,4 +1,43 @@
 package Problems.Problems_0000;
 
-public class Solution_0021 {
+import common.ListNode;
+
+class Solution_0021 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode answer = new ListNode(69);
+        ListNode answerHead = answer;
+        int loop = 0;
+
+        while(loop != 10) {
+
+            if(list1 == null) {
+                answer.next = list2;
+                break;
+            }
+
+            if(list2 == null) {
+                System.out.println("it ends here");
+                answer.next = list1;
+                break;
+            }
+
+            if(list1.val <= list2.val) {
+                answer.next = list1;
+                list1 = list1.next;
+            } else {
+                answer.next = list2;
+                list2 = list2.next;
+            }
+
+            answer = answer.next;
+            System.out.print(answer.val+" ");
+
+
+        }
+
+        return answerHead.next;
+
+    }
+
 }
