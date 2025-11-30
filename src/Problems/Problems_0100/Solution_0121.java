081c6d764000bf47f7c1df06684179d92cbe20d6
Dhaval <dhavallakhaniofficial@gmail.com>
2025-11-30 22:40:18 +0530

    Problem 0088 Runtime 1 ms Beats 99.88%
    Memory
    94.25 MB Beats 35.94%

---
+++ src/Problems/Problems_0100/Solution_0121.java
@@ -0,0 +1,28 @@
+package Problems.Problems_0100;
+
+public class Solution_0121 {
+
+    public int maxProfit(int[] prices) {
+
+        int minBuy = 10000;
+        int max = 0;
+
+
+        // [7,1,5,3,6,4]
+        for(int i=0;i<prices.length;i++) {
+
+            if(prices[i] < minBuy) {
+                minBuy = prices[i];
+            }
+
+            if(prices[i] > minBuy && prices[i]-minBuy > max) {
+                max = prices[i]-minBuy;
+            }
+
+        }
+
+        return max;
+    }
+
+
+}
