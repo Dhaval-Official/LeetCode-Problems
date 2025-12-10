a448ee2eb439a6ba9edd1d74ba98a6d93b460606
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-10 22:59:53 +0530

    Problem 0075
    Runtime
    0 ms Beats 100.00%
    
    Memory
    40.82 MB Beats 99.99%

--- src/Problems/Problems_0000/Solution_0075.java
+++ src/Problems/Problems_0000/Solution_0075.java
@@ -1,4 +1,19 @@
 package Problems.Problems_0000;
 
 public class Solution_0075 {
+
+    public void sortColors(int[] nums) {
+        int red=0,white=0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i] == 0) {red++;}
+            else if(nums[i] == 1) {white++;}
+
+        }
+        for(int i=0;i<nums.length;i++) {
+            if(red-- > 0) nums[i] = 0;
+            else if(white-- > 0) nums[i] = 1;
+            else nums[i] = 2;
+
+        }
+    }
 }
