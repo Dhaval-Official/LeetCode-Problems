adea3603fc61d0bdd4cf3dedbf8b0e5cfbb888fd
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-10 22:56:54 +0530

    Problem 0074
    Runtime
    0 ms Beats 100.00%
    Memory
    40.65 MB Beats 100.00%

--- src/Problems/Problems_0000/Solution_0074.java
+++ src/Problems/Problems_0000/Solution_0074.java
@@ -4,7 +4,17 @@ public class Solution_0074 {
 
     public boolean searchMatrix(int[][] matrix, int target) {
 
+        for(int i=0;i<matrix.length;i++) {
 
+            if(target <= matrix[i][matrix[0].length-1] && target >= matrix[i][0]) {
+                for(int j=0;j<=matrix[0].length-1;j++) {
+                    if(matrix[i][j] == target) {
+                        return true;
+                    }
+                }
+            }
+
+        }
 
         return false;
     }
