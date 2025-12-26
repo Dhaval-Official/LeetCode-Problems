52dd2c1f60d5be2985533f1c07c8c30c364e5cb4
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-26 22:41:06 +0530

    Problem 1470 Starter

---
+++ src/Problems/Problems_1400/Solution_1470.java
@@ -0,0 +1,16 @@
+package Problems.Problems_1400;
+
+public class Solution_1470 {
+    public int[] shuffle(int[] nums, int n) {
+
+        int[] ans = new int[nums.length];
+        int halfSize = (nums.length-1)/2;
+        int ansptr;
+
+        for (int i = 0; i < halfSize; i++) {
+            
+        }
+
+        return ans;
+    }
+}
