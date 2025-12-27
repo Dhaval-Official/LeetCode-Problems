0b480f9bd842937229b5b7350580de4770598c83
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-28 00:14:07 +0530

    Problem 1365 Runtime 8 ms Beats 60.16%
    Memory
    45.90 MB Beats 19.64%

--- src/Problems/Problems_1300/Solution_1365.java
+++ src/Problems/Problems_1300/Solution_1365.java
@@ -1,4 +1,21 @@
 package Problems.Problems_1300;
 
 public class Solution_1365 {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] ans = new int[nums.length];
+        int k;
+        for (int i = 0; i < nums.length; i++) {
+            k=0;
+            for (int j = 0; j < nums.length; j++) {
+                if(nums[i] > nums[j]) {
+                    k++;
+                }
+            }
+            ans[i] = k;
+        }
+
+        return ans;
+
+    }
 }
