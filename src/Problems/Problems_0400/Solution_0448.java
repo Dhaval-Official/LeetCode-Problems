4dbc9e4366f69c22e2d53dc5d2f5674f2b14e7c2
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-28 10:38:51 +0530

    Problem 1365 Runtime 4 ms Beats 95.50%
    Memory
    70.76 MB Beats 35.61%

---
+++ src/Problems/Problems_0400/Solution_0448.java
@@ -0,0 +1,24 @@
+package Problems.Problems_0400;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_0448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        ArrayList ans = new ArrayList<Integer>();
+        int[] nums2 = new int[nums.length];
+
+        for(int i=0;i<nums.length;i++) {
+            nums2[nums[i]-1] = nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++) {
+            if(nums2[i] == 0) {
+                ans.add(i+1);
+            }
+        }
+
+        return ans;
+    }
+}
