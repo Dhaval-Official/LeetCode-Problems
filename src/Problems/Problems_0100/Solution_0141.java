5e64011b0bd9759addf5f7eb1d5261150006afb4
Dhaval <dhavallakhaniofficial@gmail.com>
2026-01-02 23:45:09 +0530

    Problem 0141 Runtime 0 ms Beats 100.00%
    Memory
    46.79 MB Beats 50.12%

--- src/Problems/Problems_0100/Solution_0141.java
+++ src/Problems/Problems_0100/Solution_0141.java
@@ -4,4 +4,29 @@ import common.ListNode;
 
 public class Solution_0141 {
 
+    public boolean hasCycle(ListNode head) {
+
+        if(head == null || head.next == null || head.next.next == null)
+            return false;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(true) {
+
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == null || fast.next == null) {
+                return false;
+            }
+
+            if(fast.next == slow.next) {
+                return true;
+            }
+
+        }
+
+    }
+
 }
