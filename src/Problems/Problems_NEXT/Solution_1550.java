c369735a333262ad9d19861c50d505c462cb0e18
Dhaval <dhavallakhaniofficial@gmail.com>
2026-01-15 09:16:16 +0530

    Problems 1550 Runtime 0 ms Beats 100.00%
    Memory
    43.55 MB Beats 98.74%

--- src/Problems/Problems_NEXT/Solution_1550.java
+++ src/Problems/Problems_NEXT/Solution_1550.java
@@ -5,10 +5,9 @@ public class Solution_1550 {
     public boolean threeConsecutiveOdds(int[] arr) {
 
         for (int i = 0; i < arr.length-2; i++) {
-            if (arr[i]%2 == 1) {
-                if (arr[i+1]%2==1 && arr[i+2]%2==1) {
-                    return true;
-                }
+            if (arr[i]%2 == 1 && arr[i+1]%2==1 && arr[i+2]%2==1) {
+                return true;
+
             }
         }
 
