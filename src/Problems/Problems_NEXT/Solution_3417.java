f96f3e487689de1c6fae72813ee19db90f97a6a3
Dhaval <dhavallakhaniofficial@gmail.com>
2026-01-17 07:36:49 +0530

    Solution_3417 Runtime 1 ms Beats 100.00%
    Memory
    47.52 MB Beats 16.83%

--- src/Problems/Problems_NEXT/Solution_3417.java
+++ src/Problems/Problems_NEXT/Solution_3417.java
@@ -8,14 +8,30 @@ public class Solution_3417 {
     public List<Integer> zigzagTraversal(int[][] grid) {
 
         List<Integer> ans = new ArrayList<Integer>();
+        boolean check = true;
 
         for (int i = 0; i < grid.length; i++) {
 
-            for (int j = 0; j < grid[i].length; j++) {
-
-                ans.add(grid[i][j]);
-
+//            System.out.println(grid[i].length);
+            if (i%2 == 0) {
+                for (int j = 0; j < grid[i].length; j++) {
+                    if (check) {
+                        ans.add(grid[i][j]);
+                    }
+                    check = !check;
+//                    System.out.println(ans.add(grid[i][j]));
+                }
+            } else {
+                for (int j = grid[i].length-1; j >= 0 ; j--) {
+                    if (check) {
+                        ans.add(grid[i][j]);
+                    }
+                    check = !check;
+//                    System.out.println(ans.add(grid[i][j]));
+                }
             }
+
+
 
         }
 
