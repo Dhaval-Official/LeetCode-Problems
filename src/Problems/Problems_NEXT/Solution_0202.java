d4ac04b280bec6dca3d392b2bec6ccab48a7ceee
Dhaval <dhavallakhaniofficial@gmail.com>
2026-01-24 23:16:05 +0530

    Problem 0202 Runtime 1 ms Beats 76.77%
    Memory
    42.40 MB Beats 70.97%

--- src/Problems/Problems_NEXT/Solution_0202.java
+++ src/Problems/Problems_NEXT/Solution_0202.java
@@ -1,4 +1,46 @@
 package Problems.Problems_NEXT;
+
+import java.util.HashSet;
+
 //solved earlier in oldID
 public class Solution_0202 {
+    public boolean isHappy(int n) {
+
+        if(n==1 || n==7 || n==10)
+            return true;
+        else if(n < 10)
+            return false;
+
+        HashSet<Integer> myhash = new HashSet<Integer>();
+        n = newNumber(n);
+
+        do{
+
+            if(n==1 || n==7 || n==10)
+                return true;
+            else if(n < 10)
+                return false;
+
+            myhash.add(n);
+            n = newNumber(n);
+
+        }while(!(myhash.contains(n)));
+
+        return false;
+
+    }
+
+    int newNumber(int n) {
+        int total = 0;
+        int mod;
+
+        while(n>0) {
+
+            mod = n%10;
+            total+= mod * mod;
+            n = n/10;
+        }
+
+        return total;
+    }
 }
