d18059782ddc8d4bc2ce4933f1325abc016f8d96
Dhaval <dhavallakhaniofficial@gmail.com>
2026-01-25 23:51:48 +0530

    Problem 0258 Runtime 1 ms Beats 97.63%
    Memory
    42.70 MB Beats 54.81%

--- src/Problems/Problems_NEXT/Solution_0258.java
+++ src/Problems/Problems_NEXT/Solution_0258.java
@@ -1,4 +1,35 @@
 package Problems.Problems_NEXT;
 //solved earlier in oldID
 public class Solution_0258 {
+
+    public int addDigits(int num) {
+
+        if(num < 10) {
+            return num;
+        }
+
+        while(num >= 10) {
+
+            num = newNum(num);
+//            System.out.println(num);
+            if(num < 10) {
+                return num;
+            }
+
+        }
+
+        return 69;
+    }
+
+    int newNum(int num) {
+
+        int total = 0;
+        while(num > 0) {
+            total+=num%10;
+            num = num/10;
+        }
+
+        return total;
+    }
+
 }
