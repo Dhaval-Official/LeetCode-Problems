efd7e882ae25ece9caa534fc2bac7a731e4cc7d1
Dhaval <dhavallakhaniofficial@gmail.com>
2026-02-01 23:18:31 +0530

    Code Restructuring Problems_0000 folder Cleared

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-package Problems.Problems_NEXT;
-
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
-
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
--- src/Problems/All/Solution_0020.java
+++ src/Problems/All/Solution_0020.java
@@ -1,4 +1,4 @@
-package Problems.Problems_0000;
+package Problems.All;
 
 import java.util.Stack;
 
