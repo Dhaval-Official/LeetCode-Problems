f29d9ee8d225f16d5f240d3c98db2e35c4484f34
Dhaval <dhavallakhaniofficial@gmail.com>
2026-02-10 06:28:36 +0530

    Solution 2351 Runtime 0 ms Beats 100.00%
    Memory
    42.25 MB Beats 99.47%

---
+++ src/Problems/All/Solution_2351.java
@@ -0,0 +1,22 @@
+package Problems.All;
+
+import java.util.HashSet;
+
+public class Solution_2351 {
+    public char repeatedCharacter(String s) {
+
+        HashSet<Character> hs = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hs.contains(s.charAt(i))) {
+                return s.charAt(i);
+            } else {
+                hs.add(s.charAt(i));
+            }
+        }
+
+        return '@';
+
+    }
+
+}
