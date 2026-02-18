fedb9f595ae5d1a7b1c3fb2e567258013c52e1e9
Dhaval <dhavallakhaniofficial@gmail.com>
2026-02-19 05:19:39 +0530

    Solution 2651 0 ms Beats 100.00%
    Memory
    42.80 MB Beats 9.51%

---
+++ src/Problems/All/Solution_2651.java
@@ -0,0 +1,10 @@
+package Problems.All;
+
+public class Solution_2651 {
+
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+
+        return (arrivalTime + delayedTime) % 24;
+
+    }
+}
