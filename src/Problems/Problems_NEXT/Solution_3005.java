606034b44e77022dce5199c193ae17c28fc95dd3
Dhaval <dhavallakhaniofficial@gmail.com>
2026-02-21 23:39:40 +0530

    Solution 3005 Runtime 3 ms Beats 30.53%
    Memory
    44.22 MB Beats 32.17%

--- src/Problems/Problems_NEXT/Solution_3005.java
+++ src/Problems/Problems_NEXT/Solution_3005.java
@@ -1,11 +1,47 @@
 package Problems.Problems_NEXT;
 
+import java.util.HashMap;
+
 public class Solution_3005 {
 
     public int maxFrequencyElements(int[] nums) {
 
-        return 69;
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        int maxvalue = 0;
+        int maxKeyCount = 0;
+
+        for (int i: nums){
+            hm.put(i, hm.getOrDefault(i,0)+1);
+        }
+
+//        System.out.println(hm);
+
+        for (int i: hm.keySet()) {
+            if (hm.get(i) > maxvalue) {
+                maxvalue = hm.get(i);
+            }
+        }
+
+        for (int i: hm.keySet()) {
+            if (hm.get(i) == maxvalue) {
+                maxKeyCount+=maxvalue;
+            }
+        }
+
+        return maxKeyCount;
 
     }
 
 }
+/*
+* Runtime
+3
+ms
+Beats
+30.53%
+Analyze Complexity
+Memory
+44.22
+MB
+Beats
+32.17%*/
