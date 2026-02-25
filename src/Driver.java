534a3de4eaf546af2b636bca19b4fc7e7007c1d5
Dhaval <dhavallakhaniofficial@gmail.com>
2026-02-25 23:05:10 +0530

    Driver Upgrade

--- src/Driver.java
+++ src/Driver.java
@@ -1,56 +1,60 @@
+import Problems.All.Solution_3560;
 import Problems.Problems_NEXT.*;
-import Problems.All.*;
+//import Problems.Problems_NEXT.*;
 import common.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Driver {
 
     public static void main(String[] args) {
 
-        ListNode ll0 = new ListNode(0);
-        ListNode ll1 = new ListNode(1);
-        ListNode ll2 = new ListNode(2);
-        ListNode ll3 = new ListNode(3);
-        ListNode ll4 = new ListNode(4);
-        ListNode ll5 = new ListNode(5);
+        ListNode ll0 = new ListNode(1);
+        ListNode ll1 = new ListNode(2);
+        ListNode ll2 = new ListNode(7);
+        ListNode ll3 = new ListNode(2);
+        ListNode ll4 = new ListNode(1);
+        ListNode ll5 = new ListNode(1);
         ListNode ll6 = new ListNode(6);
         ListNode ll7 = new ListNode(7);
         ListNode ll8 = new ListNode(8);
-
-        ListNode llA = new ListNode(-69);
-        ListNode llB = new ListNode(-69);
 
         ll0.next = ll1;
         ll1.next = ll2;
         ll2.next = ll3;
         ll3.next = ll4;
-        ll4.next = ll5;
-        ll5.next = ll6;
-        ll6.next = ll7;
+//        ll4.next = ll5;
+//        ll5.next = ll6;
+//        ll6.next = ll7;
         ll7.next = ll8;
 
         System.out.println();
-        ListNode ptr = llA.next;
+//        ListNode ptr = llA.next;
 //        while (ptr.next != null) {
 //            System.out.println(ptr.val);
 //            ptr = ptr.next;
 //        }
-        int[] arr = {0,5,3};
+        ArrayList<Integer> al = new ArrayList<>(List.of(2,5,7,8,9,2,3,4,3,1));
+        int[] arr = {2,5,7,8,9,2,3,4,3,1};
         String[] strArr = {"abc","car","ada","racecar","cool"};
         int[] target = {3,7,11,9};
         int[][] arr2 = {{1,2,3},{4,5,6},{7,8,9}};
+        String str = "PAHNAPLSIIGYIR";
 
-        Solution_0190 s =
-                new Solution_0190();
+        Solution_3560 s =
+                new Solution_3560();
 
         System.out.println();
-        System.out.println(
-                s.reverseBits(43261596)
+        System.out.println("\nANS: "+
+                s.minCuttingCost(6,5,5)
         );
 //        System.out.println(res[0]);
 //        for (int i = 0; i < res.length; i++) {
 //            System.out.print(res[i]+", ");
 //        }
 
+        Solution_9999 s9 = new Solution_9999();
     }
 
 }
