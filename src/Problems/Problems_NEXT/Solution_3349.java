5373ac50e93e7b487eb006f96cce3d1c70fd4dbb
Dhaval <dhavallakhaniofficial@gmail.com>
2026-02-26 23:48:44 +0530

    New Problems addition

--- src/Problems/Problems_NEXT/Solution_3349.java
+++ src/Problems/Problems_NEXT/Solution_3349.java
@@ -1,9 +1,37 @@
 package Problems.Problems_NEXT;
 
+import java.util.List;
+
 public class Solution_3349 {
 
-    public int maxFrequencyElements(int[] nums) {
-        return 0;
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+
+        int Bptr = k;
+        int Aptr = 0;
+        int tries =0;
+
+        boolean AFound = true, BFound = true;
+
+        for (int i=0; i < nums.size()-k; i++) {
+            int j=i;
+            int kFiller =0;
+            while (kFiller < k) {
+                if (nums.get(i) > nums.get(j)) {
+                    break;
+                } else {
+                    System.out.println(nums.get(i) +" "+ nums.get(j));
+                }
+                j++;
+                kFiller++;
+            }
+            if (kFiller == k) {
+                break;
+            }
+        }
+
+
+
+        return false;
     }
 
 }
