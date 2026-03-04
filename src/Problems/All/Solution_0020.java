cf23a57a35a0d2ec5f32dcf1755097fcca5b08d4
Dhaval <dhavallakhaniofficial@gmail.com>
2026-03-04 23:31:58 +0530

    Solution 0020 Runtime 4 ms Beats 42.16%
    Memory
    43.37 MB Beats 57.86%

--- src/Problems/All/Solution_0020.java
+++ src/Problems/All/Solution_0020.java
@@ -5,48 +5,41 @@ import java.util.Stack;
 public class Solution_0020 {
     public boolean isValid(String s) {
 
-        if (s.charAt(0) == ')' | s.charAt(0) == ']' | s.charAt(0) == '}') {
+        if(s.charAt(0)=='}' || s.charAt(0)==')' || s.charAt(0)==']') {
             return false;
         }
+        char invert='0';
+        Stack st = new Stack<Character>();
+        // st.push(s.charAt(0));
+        for(int i=0;i<s.length();i++) {
 
-        Stack<Character> st = new Stack<Character>();
-//        System.out.println("Early "+ st);
-        for (int i = 0; i < s.length(); i++) {
-
-//            ch = s.charAt(i);
-            if (s.charAt(i) == '(' | s.charAt(i) == '[' | s.charAt(i) == '{') {
-                st.push(s.charAt(i));
-            } else {
+            invert='0';
+            if(s.charAt(i)=='}') {
+                invert = '{';
+            }
+            if(s.charAt(i)==']') {
+                invert = '[';
+            }
+            if(s.charAt(i)==')') {
+                invert = '(';
+            }
+            if(invert != '0') {
                 if(st.empty()) {
                     return false;
-                } else {
-                    if (s.charAt(i) == ')' ) {
-                        if (st.peek() == '(') {
-                            st.pop();
-                        } else {
-                            return false;
-                        }
-                    } else if (s.charAt(i) == ']' ) {
-                        if (st.peek() == '[' ) {
-                            st.pop();
-                        } else {
-                            return false;
-                        }
-                    } else {
-                        if (st.peek() == '{') {
-                            st.pop();
-                        } else {
-                            return false;
-                        }
-                    }
+                }
+                if(invert == (Character) st.peek()) {
+                    st.pop();
+                    continue;
                 }
             }
-//
+
+
+            // System.out.print(s.charAt(i)+" ");
+            st.push(s.charAt(i));
+
+
         }
-
-//        System.out.println(st);
-//        System.out.println("Final Answer");
+        // System.out.println(st);
         return st.empty();
-
     }
 }
