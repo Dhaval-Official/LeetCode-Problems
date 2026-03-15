7edbb3eae9d678965ee943181c5ded02a8391015
Dhaval <dhavallakhaniofficial@gmail.com>
2026-03-15 22:45:00 +0530

    Solution 1295 Runtime 1 ms Beats 99.49%
    Memory
    42.20 MB Beats 99.99%

--- src/Problems/This_Week/Solution_1295.java
+++ src/Problems/This_Week/Solution_1295.java
@@ -1,6 +1,29 @@
 package Problems.This_Week;
 
 public class Solution_1295 {
+
+    public int findNumbers(int[] nums) {
+
+        int totalEven = 0;
+        boolean isEven;
+
+        for(int i=0;i<nums.length;i++) {
+
+            isEven = true;
+            while(nums[i] > 0) {
+                nums[i] = nums[i]/10;
+                isEven = !isEven;
+            }
+
+            if(isEven) {
+                totalEven++;
+            }
+
+        }
+
+        return totalEven;
+
+    }
+
 }
 
-//Easy Solved
