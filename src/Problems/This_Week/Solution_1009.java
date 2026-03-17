5a92196d82f6847d12c984054a8158dd0766adb5
Dhaval <dhavallakhaniofficial@gmail.com>
2026-03-18 00:01:58 +0530

    Solution 1009 Runtime 1 ms Beats 24.48%
    Memory
    41.83 MB Beats 92.37%

---
+++ src/Problems/This_Week/Solution_1009.java
@@ -0,0 +1,40 @@
+package Problems.This_Week;
+
+public class Solution_1009 {
+
+    public int bitwiseComplement(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(n>0) {
+
+            int binary = n%2;
+            n = n/2;
+
+            if(binary == 0) {
+                sb.append('1');
+            } else {
+                sb.append('0');
+            }
+
+        }
+
+//        System.out.println(sb);
+        int ans = sb.charAt(0) == '1' ? 1 : 0;
+        int pow = 1;
+
+        for (int i=1; i<sb.length();i++) {
+            pow *= 2;
+            if (sb.charAt(i) == '1') {
+                ans += pow;
+            }
+        }
+
+        return ans;
+    }
+
+}
