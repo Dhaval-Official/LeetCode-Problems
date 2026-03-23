4ea653b347073c808bddbe77f18569fa8bbf1e02
Dhaval <dhavallakhaniofficial@gmail.com>
2026-03-23 23:03:26 +0530

    New Problem Addition

--- src/Problems/This_Week/Solution_1598.java
+++ src/Problems/This_Week/Solution_1598.java
@@ -1,11 +1,27 @@
 package Problems.This_Week;
 
+import java.util.Objects;
+
 public class Solution_1598 {
 
     public int minOperations(String[] logs) {
 
+        int loc = 0;
 
-        return 69;
+        for (String log: logs) {
+            if (Objects.equals(log, "./")) {
+                continue;
+            } else if (Objects.equals(log, "../")) {
+                if (loc > 0) {
+                    loc--;
+                }
+            } else {
+                loc++;
+            }
+        }
+
+
+        return loc;
     }
 
 }
